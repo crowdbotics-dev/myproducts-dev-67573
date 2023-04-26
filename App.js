313e010443ce356ae71d80843a58a1a397a71ef1
Aline Tavares <aline@crowdbotics.com>
2023-04-26 13:14:51 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
